2f23e004e060266e628a55ef00977be08806fd87
aryakrishna0521 <aryakrishna0521@gmail.com>
2025-12-12 16:59:21 +0530

    removed validation of loyalty

--- posawesome/public/js/posapp/components/pos/Payments.vue
+++ posawesome/public/js/posapp/components/pos/Payments.vue
@@ -1418,14 +1418,13 @@ export default {
   },
 
   watch: {
- 
-    
+    loyalty_amount(value) {
       if (value > this.available_pioints_amount) {
         this.invoice_doc.loyalty_amount = 0;
         this.invoice_doc.redeem_loyalty_points = 0;
         this.invoice_doc.loyalty_points = 0;
         evntBus.$emit("show_mesage", {
-          text: `Loyalty Amount cannot be more than ${this.available_pioints_amount}`,
+          text: `Loyalty Amount can not be more then ${this.available_pioints_amount}`,
           color: "error",
         });
       } else {
